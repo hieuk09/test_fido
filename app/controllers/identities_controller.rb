0357cc6919bea01b8ae38119e79b70efd5fac6ea
hieuk09 <hieuk09@gmail.com>
2022-08-12 15:33:08 +0800

    Make challenge from BE work

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,6 +1,12 @@
 class IdentitiesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     identity = Identity.create(uid: params[:uid])
-    render json: identity.as_json
+    challenge = Base64.strict_encode64(SecureRandom.random_bytes(32))
+    render json: {
+      identity: identity,
+      challenge: challenge
+    }
   end
 end
